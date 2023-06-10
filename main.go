5c50ab605639853bf28624d29eb8f37dcc3b266e
Zpuspokusumo <zpuspokusumo@gmail.com>
2023-06-10 20:02:03 +0700

    fixed scope of time for random gen

--- main.go
+++ main.go
@@ -11,10 +11,10 @@ import (
 func main() {
 	server := gin.Default()
 
-	handom := rand.NewSource(time.Now().UnixNano())
-	name := services.Sktnamegen(handom)
-
 	server.GET("/test", func(ctx *gin.Context) {
+		handom := rand.NewSource(time.Now().UnixNano())
+		name := services.Sktnamegen(handom)
+		//time := time.Now().UnixNano()
 		ctx.JSON(200, gin.H{
 			"message": name,
 		})
